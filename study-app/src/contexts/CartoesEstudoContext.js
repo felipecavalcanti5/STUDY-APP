5ff1542e0ba772562ee6686019bbdb0dd8481f9d
Felipe Cavalcanti <felipe.araujo@elevaty.com.br>
2024-11-26 23:35:03 -0300

    study-app 1

---
+++ study-app/src/contexts/CartoesEstudoContext.js
@@ -0,0 +1,46 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Children, createContext, useEffect, useState } from "react";
+
+
+const CartoesEstudoContext = createContext()
+
+export const CartoesEstudoProvider = ({ children }) => {
+
+    const [cartoes, setCartoes] = useState([])
+
+    useEffect(() => {
+        carregarCartoes()
+    }, [])
+
+    const carregarCartoes = async () => {
+        const cartoesArmazenados = await AsyncStorage.getItem('cartoes')
+        if (cartoesArmazenados) setCartoes(JSON.parse(cartoesArmazenados))
+    }
+
+    const adicionarCartao = async (cartao) => {
+        const novosCartoes = [...cartoes, {...cartao, id: Date.now() }]
+        setCartoes(novosCartoes)
+        await AsyncStorage.setItem('cartoes', JSON.stringify(novosCartoes))
+    }
+
+    const atualizarCartoes = async (id, atualizacoes) => {
+        const novosCartoes = cartoes.map(cartao => cartao.id ===id ? {
+                ...cartao, ...atualizacoes} : cartao)
+        setCartoes(novosCartoes)
+        await AsyncStorage.setItem('cartoes', JSON.stringify(novosCartoes))
+    }
+
+    const excluirCartao = async (id) => {
+        const novosCartoes = cartoes.filter(cartao => cartao.id !== id)
+        setCartoes(novosCartoes)
+        await AsyncStorage.setItem('cartoes', JSON.stringify(novosCartoes))
+    }
+
+    return (
+        <CartoesEstudoContext.Provider value={{ cartoes, adicionarCartao, atualizarCartoes, excluirCartao}}>
+            { children }
+        </CartoesEstudoContext.Provider>    
+    )
+}
+
+export default CartoesEstudoContext
